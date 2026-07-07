12087c3335dea299dc015be53a29b51f49205955
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-07-08 04:20:24 +0600

    feat: setup Stripe client configuration

---
+++ src/config/stripe.ts
@@ -0,0 +1,7 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2024-06-20' as Stripe.LatestApiVersion,
+});
+
+export default stripe;
